c53eb1081fb8d077f82b2f77199e23b70690ea1e
konfuzz <konfuzz@gmail.com>
2026-05-22 21:48:07 +0400

    feat: add theme selection feature

---
+++ src/context/AppContext.ts
@@ -0,0 +1,8 @@
+import { createContext } from 'react';
+
+interface AppContextProps {
+  isDark?: boolean;
+  toggleTheme?: () => void;
+}
+
+export const AppContext = createContext<AppContextProps>({});
