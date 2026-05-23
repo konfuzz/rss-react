f67ddcf822b2b1829d5b7c66405807e83f2b8980
konfuzz <konfuzz@gmail.com>
2026-05-23 10:09:09 +0400

    feat: create layout for the flyout

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { ErrorBoundary } from "./components/ErrorBoundary";
 import RecipeDetail from "./pages/RecipeDetail";
 import { AppContext } from "./context/AppContext";
 import { useLocalStorage } from "./hooks/useLocalStorage";
+import { Flyout } from "./components/Flyout";
 
 export default function App() {
   const [isDark, setIsDark] = useLocalStorage<boolean>("isDark", false);
@@ -32,6 +33,7 @@ export default function App() {
           <Route path="/about" element={<AboutPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
+        <Flyout />
       </AppContext.Provider>
     </ErrorBoundary>
   );
---
+++ src/components/Flyout.tsx
@@ -0,0 +1,9 @@
+export function Flyout() {
+  return (
+    <div className="flyout">
+      <div className="selected">1 selected</div>
+      <button className="unselect">Unselect All</button>
+      <a href="#" className="download-btn" download>Download</a>
+    </div>
+  )
+}
