f3501dfc892ebd181c4fcf37e7c04ac345f1b934
konfuzz <konfuzz@gmail.com>
2026-06-08 21:42:58 +0400

    feat: improve uncontrolled form

--- src/components/UncontrolledForm/UncontrolledForm.tsx
+++ src/components/UncontrolledForm/UncontrolledForm.tsx
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import { useFormStore } from '../../store/useFormStore';
 
 export default function UncontrolledForm({ onClose }: { onClose: () => void }) {
-  const { addSubmission } = useFormStore();
+  const { addSubmission, countries } = useFormStore();
   const [image, setImage] = useState<string | null>(null);
+  const [strength, setStrength] = useState<string | null>(null);
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.currentTarget.files?.[0];
@@ -13,6 +14,35 @@ export default function UncontrolledForm({ onClose }: { onClose: () => void }) {
     reader.onload = () => {
       setImage(reader.result as string);
     };
+  };
+
+  const handlePasswordStrength = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const password = e.currentTarget.value;
+    const checks = {
+      number: /\d/.test(password),
+      uppercase: /[A-Z]/.test(password),
+      lowercase: /[a-z]/.test(password),
+      special: /[^A-Za-z0-9]/.test(password),
+    };
+
+    const score = Object.values(checks).filter(Boolean).length;
+
+    switch (score) {
+      case 0:
+      case 1:
+        setStrength('weak');
+        break;
+      case 2:
+        setStrength('simple');
+        break;
+      case 3:
+        setStrength('medium');
+        break;
+      default:
+        setStrength('strong');
+        break;
+    }
+
   };
 
   const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
@@ -27,12 +57,14 @@ export default function UncontrolledForm({ onClose }: { onClose: () => void }) {
       gender: data.gender as string,
       terms: 'terms' in data ? data.terms === "On" : false,
       image: image ?? 'https://placehold.co/200x200/png',
-      password: '123456',
-      country: 'USA',
+      password: data.password as string,
+      confirm: data.confirm as string,
+      country: data.country as string,
     })
 
     onClose();
     e.currentTarget.reset();
+    setImage(null);
   };
 
   return (
@@ -53,25 +85,31 @@ export default function UncontrolledForm({ onClose }: { onClose: () => void }) {
       <div className="field">
         <label htmlFor="gender">Gender</label>
         <select id="gender" name="gender">
-          <option>Select Gender</option>
+          <option value="">Select Gender</option>
           <option value="Male">Male</option>
           <option value="Female">Female</option>
         </select>
       </div>
       <div className="field">
         <label htmlFor="country">Country</label>
-        <input type="text" id="country" name="country" />
+        <input type="text" id="country" name="country" list="сountries" />
+        <datalist id="сountries">
+          {countries.map((country) => (
+            <option key={country}>{country}</option>
+          ))}
+        </datalist>
       </div>
       <div className="field">
         <label htmlFor="image">Upload Image</label>
-        <input type="file" id="image" name="image" accept="image/*" onChange={handleImageChange} />
+        <input type="file" id="image" name="image" accept="image/png, image/jpeg" onChange={handleImageChange} />
       </div>
       <div className="field">
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" />
+        <input type="password" id="password" name="password" onChange={handlePasswordStrength} />
+        <div className="strength">{strength}</div>
       </div>
       <div className="field">
-        <label htmlFor="confirm">Country</label>
+        <label htmlFor="confirm">Confirm Password</label>
         <input type="password" id="confirm" name="confirm" />
       </div>
       <div className="field">
--- src/types/types.ts
+++ src/types/types.ts
@@ -7,6 +7,7 @@ export interface FormSubmission {
   terms: boolean;
   image: string;
   password: string;
+  confirm: string;
   country: string;
 }
 
